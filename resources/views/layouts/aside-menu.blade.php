27f4cd99e7ae0a22541a417125a1c1e779a9d123
LuisHoyos7 <luishoyosolaya@gmail.com>
2020-10-08 20:42:30 -0500

    change view modules

--- resources/views/layouts/aside-menu.blade.php
+++ resources/views/layouts/aside-menu.blade.php
@@ -18,11 +18,6 @@
 			</a>
 		</li>
 
-		<li class="menu-section">
-			<h4 class="menu-text">Almacen</h4>
-			<i class="menu-icon ki ki-bold-more-hor icon-md"></i>
-		</li>
-
 		<li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
 			<a href="{{route('category.index')}}" class="menu-link menu-toggle">
 				<span class="svg-icon menu-icon">
@@ -164,9 +159,56 @@
 				</div>
 			</li>
 
-			<li class="menu-section">
-				<h4 class="menu-text">Cartera</h4>
-				<i class="menu-icon ki ki-bold-more-hor icon-md"></i>
+
+			
+			<li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
+				<a href="{{route('category.index')}}" class="menu-link menu-toggle">
+					<span class="svg-icon menu-icon">
+						<!--begin::Svg Icon | path:assets/media/svg/icons/Layout/Layout-4-blocks.svg-->
+						<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+							<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+								<rect x="0" y="0" width="24" height="24"/>
+								<rect fill="#000000" opacity="0.3" x="7" y="4" width="10" height="4"/>
+								<path d="M7,2 L17,2 C18.1045695,2 19,2.8954305 19,4 L19,20 C19,21.1045695 18.1045695,22 17,22 L7,22 C5.8954305,22 5,21.1045695 5,20 L5,4 C5,2.8954305 5.8954305,2 7,2 Z M8,12 C8.55228475,12 9,11.5522847 9,11 C9,10.4477153 8.55228475,10 8,10 C7.44771525,10 7,10.4477153 7,11 C7,11.5522847 7.44771525,12 8,12 Z M8,16 C8.55228475,16 9,15.5522847 9,15 C9,14.4477153 8.55228475,14 8,14 C7.44771525,14 7,14.4477153 7,15 C7,15.5522847 7.44771525,16 8,16 Z M12,12 C12.5522847,12 13,11.5522847 13,11 C13,10.4477153 12.5522847,10 12,10 C11.4477153,10 11,10.4477153 11,11 C11,11.5522847 11.4477153,12 12,12 Z M12,16 C12.5522847,16 13,15.5522847 13,15 C13,14.4477153 12.5522847,14 12,14 C11.4477153,14 11,14.4477153 11,15 C11,15.5522847 11.4477153,16 12,16 Z M16,12 C16.5522847,12 17,11.5522847 17,11 C17,10.4477153 16.5522847,10 16,10 C15.4477153,10 15,10.4477153 15,11 C15,11.5522847 15.4477153,12 16,12 Z M16,16 C16.5522847,16 17,15.5522847 17,15 C17,14.4477153 16.5522847,14 16,14 C15.4477153,14 15,14.4477153 15,15 C15,15.5522847 15.4477153,16 16,16 Z M16,20 C16.5522847,20 17,19.5522847 17,19 C17,18.4477153 16.5522847,18 16,18 C15.4477153,18 15,18.4477153 15,19 C15,19.5522847 15.4477153,20 16,20 Z M8,18 C7.44771525,18 7,18.4477153 7,19 C7,19.5522847 7.44771525,20 8,20 L12,20 C12.5522847,20 13,19.5522847 13,19 C13,18.4477153 12.5522847,18 12,18 L8,18 Z M7,4 L7,8 L17,8 L17,4 L7,4 Z" fill="#000000"/>
+							</g>
+						</svg><!--end::Svg Icon-->
+						<!--end::Svg Icon-->
+					</span>
+					<span class="menu-text">Compras</span>
+					<i class="menu-arrow"></i>
+				</a>
+				<div class="menu-submenu" kt-hidden-height="80" style="display: none; overflow: hidden;">
+					<i class="menu-arrow"></i>
+						<ul class="menu-subnav">
+							<li class="menu-item menu-item-parent" aria-haspopup="true">
+								<span class="menu-link">
+									<span class="menu-text">Compras</span>
+								</span>
+							</li>
+							<li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
+								<a href="{{route('estimate.index')}}"  class="menu-link menu-toggle">
+									<span class="svg-icon menu-icon">
+										<!--begin::Svg Icon | path:assets/media/svg/icons/Layout/Layout-4-blocks.svg-->
+										<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+											<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+												<rect x="0" y="0" width="24" height="24"/>
+												<path d="M8,3 L8,3.5 C8,4.32842712 8.67157288,5 9.5,5 L14.5,5 C15.3284271,5 16,4.32842712 16,3.5 L16,3 L18,3 C19.1045695,3 20,3.8954305 20,5 L20,21 C20,22.1045695 19.1045695,23 18,23 L6,23 C4.8954305,23 4,22.1045695 4,21 L4,5 C4,3.8954305 4.8954305,3 6,3 L8,3 Z" fill="#000000" opacity="0.3"/>
+												<path d="M11,2 C11,1.44771525 11.4477153,1 12,1 C12.5522847,1 13,1.44771525 13,2 L14.5,2 C14.7761424,2 15,2.22385763 15,2.5 L15,3.5 C15,3.77614237 14.7761424,4 14.5,4 L9.5,4 C9.22385763,4 9,3.77614237 9,3.5 L9,2.5 C9,2.22385763 9.22385763,2 9.5,2 L11,2 Z" fill="#000000"/>
+												<rect fill="#000000" opacity="0.3" x="10" y="9" width="7" height="2" rx="1"/>
+												<rect fill="#000000" opacity="0.3" x="7" y="9" width="2" height="2" rx="1"/>
+												<rect fill="#000000" opacity="0.3" x="7" y="13" width="2" height="2" rx="1"/>
+												<rect fill="#000000" opacity="0.3" x="10" y="13" width="7" height="2" rx="1"/>
+												<rect fill="#000000" opacity="0.3" x="7" y="17" width="2" height="2" rx="1"/>
+												<rect fill="#000000" opacity="0.3" x="10" y="17" width="7" height="2" rx="1"/>
+											</g>
+										</svg>
+									<!--end::Svg Icon-->
+									</span>
+									<span class="menu-text">Compras</span>
+								</a>
+							</li>
+						</ul>
+					</div>
 			</li>
 
 			<li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
@@ -193,28 +235,7 @@
 									<span class="menu-text">Cotizaciones</span>
 								</span>
 							</li>
-							<li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
-								<a href="{{route('estimate.index')}}"  class="menu-link menu-toggle">
-									<span class="svg-icon menu-icon">
-										<!--begin::Svg Icon | path:assets/media/svg/icons/Layout/Layout-4-blocks.svg-->
-										<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-											<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-												<rect x="0" y="0" width="24" height="24"/>
-												<path d="M8,3 L8,3.5 C8,4.32842712 8.67157288,5 9.5,5 L14.5,5 C15.3284271,5 16,4.32842712 16,3.5 L16,3 L18,3 C19.1045695,3 20,3.8954305 20,5 L20,21 C20,22.1045695 19.1045695,23 18,23 L6,23 C4.8954305,23 4,22.1045695 4,21 L4,5 C4,3.8954305 4.8954305,3 6,3 L8,3 Z" fill="#000000" opacity="0.3"/>
-												<path d="M11,2 C11,1.44771525 11.4477153,1 12,1 C12.5522847,1 13,1.44771525 13,2 L14.5,2 C14.7761424,2 15,2.22385763 15,2.5 L15,3.5 C15,3.77614237 14.7761424,4 14.5,4 L9.5,4 C9.22385763,4 9,3.77614237 9,3.5 L9,2.5 C9,2.22385763 9.22385763,2 9.5,2 L11,2 Z" fill="#000000"/>
-												<rect fill="#000000" opacity="0.3" x="10" y="9" width="7" height="2" rx="1"/>
-												<rect fill="#000000" opacity="0.3" x="7" y="9" width="2" height="2" rx="1"/>
-												<rect fill="#000000" opacity="0.3" x="7" y="13" width="2" height="2" rx="1"/>
-												<rect fill="#000000" opacity="0.3" x="10" y="13" width="7" height="2" rx="1"/>
-												<rect fill="#000000" opacity="0.3" x="7" y="17" width="2" height="2" rx="1"/>
-												<rect fill="#000000" opacity="0.3" x="10" y="17" width="7" height="2" rx="1"/>
-											</g>
-										</svg>
-									<!--end::Svg Icon-->
-									</span>
-									<span class="menu-text">Cotizaciones</span>
-								</a>
-							</li>
+							
 						</ul>
 					</div>
 				</li>
@@ -257,14 +278,25 @@
 								</a>
 							</li>
 							<li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
-								<a href="{{route('invoice.index',['invoiceType' => 'COMPRA'])}}"  class="menu-link menu-toggle">
+								<a href="{{route('estimate.index')}}"  class="menu-link menu-toggle">
 									<span class="svg-icon menu-icon">
 										<!--begin::Svg Icon | path:assets/media/svg/icons/Layout/Layout-4-blocks.svg-->
-										<i class="flaticon2-infographic"></i>
+										<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+											<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+												<rect x="0" y="0" width="24" height="24"/>
+												<path d="M8,3 L8,3.5 C8,4.32842712 8.67157288,5 9.5,5 L14.5,5 C15.3284271,5 16,4.32842712 16,3.5 L16,3 L18,3 C19.1045695,3 20,3.8954305 20,5 L20,21 C20,22.1045695 19.1045695,23 18,23 L6,23 C4.8954305,23 4,22.1045695 4,21 L4,5 C4,3.8954305 4.8954305,3 6,3 L8,3 Z" fill="#000000" opacity="0.3"/>
+												<path d="M11,2 C11,1.44771525 11.4477153,1 12,1 C12.5522847,1 13,1.44771525 13,2 L14.5,2 C14.7761424,2 15,2.22385763 15,2.5 L15,3.5 C15,3.77614237 14.7761424,4 14.5,4 L9.5,4 C9.22385763,4 9,3.77614237 9,3.5 L9,2.5 C9,2.22385763 9.22385763,2 9.5,2 L11,2 Z" fill="#000000"/>
+												<rect fill="#000000" opacity="0.3" x="10" y="9" width="7" height="2" rx="1"/>
+												<rect fill="#000000" opacity="0.3" x="7" y="9" width="2" height="2" rx="1"/>
+												<rect fill="#000000" opacity="0.3" x="7" y="13" width="2" height="2" rx="1"/>
+												<rect fill="#000000" opacity="0.3" x="10" y="13" width="7" height="2" rx="1"/>
+												<rect fill="#000000" opacity="0.3" x="7" y="17" width="2" height="2" rx="1"/>
+												<rect fill="#000000" opacity="0.3" x="10" y="17" width="7" height="2" rx="1"/>
+											</g>
 										</svg>
 									<!--end::Svg Icon-->
 									</span>
-									<span class="menu-text">Facturas por Compras</span>
+									<span class="menu-text">Cotizaciones</span>
 								</a>
 							</li>
 						</ul>
@@ -318,11 +350,7 @@
 						</div>
 					</li>
 
-				<li class="menu-section">
-					<h4 class="menu-text">Usuarios</h4>
-					<i class="menu-icon ki ki-bold-more-hor icon-md"></i>
-				</li>
-
+				
 				<li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
 					<a href="{{route('person.index')}}" class="menu-link menu-toggle">
 						<span class="svg-icon menu-icon">
